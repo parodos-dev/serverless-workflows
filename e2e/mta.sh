2e3f07b69b8b23e2f65fa8e1e7622725504855a6
Roy Golan <rgolan@redhat.com>
2024-01-22 21:47:41 +0200

    ci: Use the orchestrator API for e2e testing
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- e2e/mta.sh
+++ e2e/mta.sh
@@ -1,38 +1,44 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-export port_forward_id=""
+# holds the pid of the port forward process for cleanups
+export port_forward_pid=""
 
 function cleanup() {
     echo "cleanup"
-    kill -9 "$port_forward_id"
+    kill -9 "$port_forward_pid"
 }
 
 function workflowDone() {
     if [[ -n "${1}" ]]; then 
-        curl -s "Content-Type: application/json" localhost:9080/MTAAnalysis/"$1"
+        id=$1
+        curl -s -H "Content-Type: application/json" localhost:9080/api/orchestrator/instances/${id} | jq -e '.state == "COMPLETED"'
     fi
 }
+
 trap 'cleanup' EXIT SIGTERM
 
-echo "Proxy MTA Analysis sonata port ⏳"
-kubectl port-forward svc/mtaanalysis 9080:80 &
-port_forward_id="$!"
+echo "Proxy Janus-idp port ⏳"
+kubectl port-forward svc/workflows-backstage 9080:7007 &
+port_forward_pid="$!"
 sleep 1
-echo "Proxy MTA Analysis sonata port ✅"
+echo "Proxy Janus-idp port ✅"
 
 echo "End to end tests start ⏳"
-id=$(curl -s -XPOST -H "Content-Type: application/json" localhost:9080/MTAAnalysis -d '{"repositoryURL": "https://github.com/spring-projects/spring-petclinic"}' | jq .id)
+id=$(curl -s -XPOST -H "Content-Type: application/json" \
+    localhost:9080/api/orchestrator/workflows/MTAAnalysis/execute \
+    -d '{"repositoryURL": "https://github.com/spring-projects/spring-petclinic"}' \
+    | jq .id)
 
-retries=3
+retries=20
+sleepduration=5
 until eval "test $retries -eq 0 || workflowDone $id"; do
-  echo "checking workflow $id is done"
-  sleep 3
+  echo "checking workflow $id completed successfully"
+  sleep 5
   retries=$((retries-1))
 done
 
 echo "End to end tests passed ✅"
 exit 0
-
 
