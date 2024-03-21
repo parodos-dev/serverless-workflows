2bdfc0b42d20463656069880eea19f52363b6f5c
Moti Asayag <masayag@redhat.com>
2024-03-21 23:40:54 +0200

    Use released version of kn cli
    The released version of kn cli integrates several plugins in it,
    including the kn-workflow plugin.
    
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- scripts/gen_manifests.sh
+++ scripts/gen_manifests.sh
@@ -2,16 +2,15 @@
 
 WORKFLOW_ID=$1
 
-if [ ! -f kn-workflow ]; then
+if [ ! -f kn ]; then
   echo "Installing kn-workflow CLI"
-  # TODO Update to released version
-  curl -L https://github.com/rgolangh/kie-tools/releases/download/0.0.2/kn-workflow-linux-amd64 -o kn-workflow
-  chmod +x kn-workflow
+  KN_CLI_URL="https://mirror.openshift.com/pub/openshift-v4/clients/serverless/latest/kn-linux-amd64.tar.gz"
+  curl -L "$KN_CLI_URL" | tar -xz && chmod +x kn-linux-amd64 && mv kn-linux-amd64 kn
 else 
-  echo "kn-workflow already available"
+  echo "kn cli already available"
 fi
 
 cd "${WORKFLOW_ID}" || exit
 # TODO Update to use --skip-namespace when the following is released
 # https://github.com/apache/incubator-kie-tools/pull/2136
-../kn-workflow gen-manifest --namespace ""
+../kn workflow gen-manifest --namespace ""
