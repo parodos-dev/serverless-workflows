2476ac77119f4f20f1f35aae4ad8f68173740734
Kamlesh Panchal <kpanchal@redhat.com>
2024-05-11 14:43:06 -0400

    Minor changes to test to see results

--- e2e/mtav6v7.sh
+++ e2e/mtav6v7.sh
@@ -11,40 +11,37 @@ function cleanup() {
     kill "$port_forward_pid" || true
 }
 
+function workflowDone() {
+    if [[ -n "${1}" ]]; then 
+        id=$1
+        curl -s -H "Content-Type: application/json" localhost:9080/api/orchestrator/instances/"${id}" | jq -e '.state == "COMPLETED"'
+    fi
+}
+
 trap 'cleanup' EXIT SIGTERM
 
-echo "Proxy MTA Analysis port ⏳"
-kubectl port-forward "$(kubectl get svc mta-analysis -o name)" 8080:80 &
+echo "Proxy Janus-idp port ⏳"
+kubectl port-forward "$(kubectl get svc -l app.kubernetes.io/component=backstage -o name)" 9080:7007 &
 port_forward_pid="$!"
 sleep 3
-echo "Proxy MTA Analysis port ✅"
+echo "Proxy Janus-idp port ✅"
 
 echo "End to end tests start ⏳"
 
-resp=$(curl --location 'http://localhost:8080/mta-analysis' --header 'Accept: application/json, text/plain, */*' --header 'Content-Type: application/json' --data '{ "repositoryURL": "https://github.com/spring-projects/spring-petclinic", "exportToIssueManager": "false", "migrationStartDatetime" : "2024-07-01T00:00:00Z", "migrationEndDatetime" : "2024-07-31T00:00:00Z"}')
-
-id=$(echo "$resp" | jq ".id")
+out=$(curl -XPOST -H "Content-Type: application/json"  http://localhost:9080/api/orchestrator/workflows/mta-analysis/execute -d '{"repositoryURL": "https://github.com/spring-projects/spring-petclinic", "exportToIssueManager": "false", "migrationStartDatetime" : "2024-07-01T00:00:00Z", "migrationEndDatetime" : "2024-07-31T00:00:00Z"}')
+id=$(echo "$out" | jq -e .id)
 
 if [ -z "$id" ] || [ "$id" == "null" ]; then
     echo "workflow instance id is null... exiting "
     exit 1
 fi
 
-mta_analysis_pod=$(kubectl get pod -o name | grep mta-analysis)
 retries=20
-until eval "test ${retries} -eq 0"; do
-  analysis_out=$(kubectl logs "$mta_analysis_pod")
-  success_result="\"mtaAnalysisResultURL\" : \"http://tackle-ui.my-konveyor-operator.svc.cluster.local:8080/hub/applications"
-  if grep -q "${success_result}" <<< "$analysis_out"
-  then
-    echo "End to end tests passed ✅"
-    exit 0
-  else
-    echo "checking workflow ${id} completed successfully"
-    sleep 30
-    retries=$((retries-1))
-  fi
+until eval "test ${retries} -eq 0 || workflowDone $id"; do
+  echo "checking workflow ${id} completed successfully"
+  sleep 5
+  retries=$((retries-1))
 done
 
-echo "Analysis did not finish in stipulated time ❌"
+echo "End to end tests passed ✅"
 
