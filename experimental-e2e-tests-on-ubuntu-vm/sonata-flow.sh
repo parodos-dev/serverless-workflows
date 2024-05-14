6fe14a6d0443fa5c60d3ddb15b6f2864d94c310c
Kamlesh Panchal <kpanchal@redhat.com>
2024-05-14 09:59:19 -0400

    Further changes to local ci

--- experimental-e2e-tests-on-ubuntu-vm/sonata-flow.sh
+++ experimental-e2e-tests-on-ubuntu-vm/sonata-flow.sh
@@ -10,13 +10,17 @@ kubectl create namespace sonataflow-infra
 kubectl create secret generic sonataflow-psql-postgresql --from-literal=postgres-username=postgres --from-literal=postgres-password=postgres -n sonataflow-infra
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install sonataflow-psql bitnami/postgresql -f ../e2e/resources/psql-values.yaml -n sonataflow-infra
-kubectl wait --for=jsonpath='{.status.phase}'=Running "$(kubectl get pod -o name | grep sonataflow-psql-postgresql)" --timeout=300s
+sleep 60
+kubectl wait --for=jsonpath='{.status.phase}'=Running "$(kubectl get pod -o name -n sonataflow-infra | grep sonataflow-psql-postgresql)" --timeout=300s -n sonataflow-infra
 
 # Sonataflow operator
+# kubectl create namespace sonataflow-operator-system
+kubectl create -f https://raw.githubusercontent.com/kiegroup/kogito-serverless-operator/main/operator.yaml
 kubectl apply -f ../e2e/resources/sonata-flow-operator.yaml
 kubectl wait --for=jsonpath='{.status.phase}'=Running "$(kubectl get pod -o name -n sonataflow-operator-system | grep sonataflow-operator-controller-manager)" -n sonataflow-operator-system  --timeout=300s
 
 # Core services of Sonataflow
 kubectl apply -f ../e2e/resources/sonata-flow-platform.yaml
-kubectl wait --for=jsonpath='{.status.phase}'=Running "$(kubectl get pod -o name | grep sonataflow-platform-jobs-service)" --timeout=300s
-kubectl wait --for=jsonpath='{.status.phase}'=Running "$(kubectl get pod -o name | grep sonataflow-platform-data-index-service)" --timeout=300s
+sleep 60
+kubectl wait --for=jsonpath='{.status.phase}'=Running "$(kubectl get pod -o name -n sonataflow-infra | grep sonataflow-platform-jobs-service)" -n sonataflow-infra --timeout=300s
+kubectl wait --for=jsonpath='{.status.phase}'=Running "$(kubectl get pod -o name -n sonataflow-infra | grep sonataflow-platform-data-index-service)" -n sonataflow-infra --timeout=300s
