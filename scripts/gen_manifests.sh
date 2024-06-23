62fc922d9f3db50fc58cdd1e2679b31e76fcd026
Moti Asayag <masayag@redhat.com>
2024-06-23 14:06:20 +0300

    Fix makefile build (#268)

--- scripts/gen_manifests.sh
+++ scripts/gen_manifests.sh
@@ -9,7 +9,7 @@ fi
 
 if [ ! -f kn ]; then
   echo "Installing kn-workflow CLI"
-  KN_CLI_URL="https://mirror.openshift.com/pub/openshift-v4/clients/serverless/latest/kn-linux-amd64.tar.gz"
+  KN_CLI_URL="https://mirror.openshift.com/pub/openshift-v4/clients/serverless/1.11.2/kn-linux-amd64.tar.gz"
   curl -L "$KN_CLI_URL" | tar -xz --no-same-owner && chmod +x kn-linux-amd64 && mv kn-linux-amd64 kn
 else 
   echo "kn cli already available"
@@ -28,10 +28,28 @@ if [ "$ENABLE_PERSISTENCE" = false ]; then
   exit
 fi
 
-SONATAFLOW_CR=manifests/01-sonataflow_${WORKFLOW_ID}.yaml
+# Find the workflow file with .sw.yaml suffix since kn-cli uses the ID to generate resource names
+workflow_file=$(printf '%s\n' ./*.sw.yaml 2>/dev/null | head -n 1)
+
+# Check if the workflow_file was found
+if [ -z "$workflow_file" ]; then
+  echo "No workflow file with .sw.yaml suffix found."
+  exit 1
+fi
+
+# Extract the 'id' property from the YAML file and convert to lowercase
+workflow_id=$(grep '^id:' "$workflow_file" | awk '{print $2}' | tr '[:upper:]' '[:lower:]')
+
+# Check if the 'id' property was found
+if [ -z "$workflow_id" ]; then
+  echo "No 'id' property found in the workflow file."
+  exit 1
+fi
+
+SONATAFLOW_CR=manifests/01-sonataflow_${workflow_id}.yaml
 yq --inplace eval '.metadata.annotations["sonataflow.org/profile"] = "prod"' "${SONATAFLOW_CR}"
 
-yq --inplace ".spec.podTemplate.container.image=\"quay.io/orchestrator/serverless-workflow-${WORKFLOW_ID}:latest\"" "${SONATAFLOW_CR}"
+yq --inplace ".spec.podTemplate.container.image=\"quay.io/orchestrator/serverless-workflow-${workflow_id}:latest\"" "${SONATAFLOW_CR}"
 
 yq --inplace ".spec |= (
   . + {
