9f9bf19e6103c609c9d90e8dc3bfbc3026451ed3
gabriel-farache <gfarache@redhat.com>
2024-10-09 16:34:56 +0200

    Update CI to merge automated PR is one already exists for the workflow
    Signed-off-by: gabriel-farache <gfarache@redhat.com>

---
+++ scripts/create_automated_m2k_kfunc_pr.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+USER_EMAIL=$1
+USER_NAME=$2
+WORKDIR=$3
+WORKFLOW_ID=$4
+PR_OR_COMMIT_URL=$5
+GH_TOKEN=$6
+WF_CONFIG_REPO=$7
+INPUT_VALUES_FILEPATH=$8
+
+git config --global user.email "${USER_EMAIL}"
+git config --global user.name "${USER_NAME}"
+gh repo clone "${WF_CONFIG_REPO}" config-repo
+cd config-repo || exit
+git switch -c "${WORKFLOW_ID}"-autopr-"${RANDOM}"
+
+./../hack/bump_chart_version.sh "${WORKFLOW_ID}" --bump-tag-version
+
+cp "${INPUT_VALUES_FILEPATH}" charts/move2kube/values.yaml
+
+mkdir -p charts/"${WORKFLOW_ID}"/templates
+cp "${WORKDIR}"/"${WORKFLOW_ID}"/manifests/* charts/"${WORKFLOW_ID}"/templates
+git add -A
+
+git commit -m "(m2k-kfunc) Automated PR"
+echo "Automated PR from ${PR_OR_COMMIT_URL}" | git commit --amend --file=-
+git remote set-url origin https://"${GH_TOKEN}"@github.com/"${WF_CONFIG_REPO}"
+git push origin HEAD
+gh pr create -f --title "m2k-kfunc: Automatic manifests generation" \
+--body "
+Updating generated manifests for m2k-kfunc application
+
+This PR was created automatically as a result of merging ${PR_OR_COMMIT_URL}
+"
---
+++ scripts/create_automated_pr.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+USER_EMAIL=$1
+USER_NAME=$2
+WORKDIR=$3
+WORKFLOW_ID=$4
+PR_OR_COMMIT_URL=$5
+GH_TOKEN=$6
+WF_CONFIG_REPO=$7
+
+git config --global user.email "${USER_EMAIL}"
+git config --global user.name "${USER_NAME}"
+gh repo clone "${WF_CONFIG_REPO}" config-repo
+cd config-repo || exit
+git switch -c "${WORKFLOW_ID}"-autopr-"${RANDOM}"
+
+./../hack/bump_chart_version.sh "${WORKFLOW_ID}" --bump-tag-version
+mkdir -p charts/"${WORKFLOW_ID}"/templates
+cp "${WORKDIR}"/"${WORKFLOW_ID}"/manifests/* charts/"${WORKFLOW_ID}"/templates
+git add -A
+
+git commit -m "(${WORKFLOW_ID}) Automated PR"
+echo "Automated PR from $PR_OR_COMMIT_URL" | git commit --amend --file=-
+git remote set-url origin https://"${GH_TOKEN}"@github.com/"${WF_CONFIG_REPO}"
+git push origin HEAD
+gh pr create -f --title "${WORKFLOW_ID}: Automatic manifests generation" \
+--body "
+Updating generated manifests for ${WORKFLOW_ID} workflow
+
+This PR was created automatically as a result of merging ${PR_OR_COMMIT_URL}
+"
